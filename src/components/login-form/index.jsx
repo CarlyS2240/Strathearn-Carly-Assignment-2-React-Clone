5860d479028b9009c100ac719509f40250d4166c
CarlyS2240 <carly.strathearn@carleton.ca>
2021-11-14 18:43:43 -0500

    Created a login component for the Login Page

---
+++ src/components/login-form/index.jsx
@@ -0,0 +1,27 @@
+import "./styles.css"
+
+import {Visibility} from '@material-ui/icons';
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+
+export const LoginForm = () => {
+
+    return (
+            <form className="loginFormContainer">
+                <label className="emailForm">
+                    <input className="emailInput" name="submitted-email" autocomplete="email" placeholder="Email Address or verified mobile phone number"></input>
+                    <hr className="inputDivider"></hr>
+                    <p>Alternative login: <span className="alternativeLogin">Alternative login with a one-time code</span></p>
+                </label>
+                <label className="passwordForm">
+                    <div className="passwordField">
+                        <input className="passwordInput" name="submitted-password" autocomplete="password" placeholder="Password"></input>
+                        <Visibility></Visibility>
+                    </div>
+                    <hr className="inputDivider"></hr>
+                    <p>Forgot your password?</p>
+                </label>
+        </form>
+   
+    );
+}
--- src/components/pages/login/index.jsx
+++ src/components/pages/login/index.jsx
@@ -1,9 +1,20 @@
 import "./styles.css";
 
 import { LoginInstructions } from '../../login-instructions/index';
+import { LoginForm } from '../../login-form/index';
+import { ArrowBack } from "@material-ui/icons";
 
 export const LoginPage = () => {
     return (
-        <LoginInstructions></LoginInstructions>
+        <>
+        <div class="loginPageContainer">
+            <div className="row1">
+                <LoginInstructions></LoginInstructions>
+            </div>
+            <div className="row2">
+                <LoginForm></LoginForm>
+            </div>
+        </div>
+        </>
     )
 }
